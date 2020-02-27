4bd01443579cee418000e3590bba735a04fa2d11
jb087 <jb087@op.pl>
2020-02-27 11:43:49 +0100

    # Added form task # Added onChange method # Improvement -> onSubmit

--- taskmanager/src/App.js
+++ taskmanager/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import FormTask from "./components/FormTask"
 
 function App() {
     return (
         <div className="App">
-            <h1>Hello World!</h1>
+            <FormTask/>
         </div>
     );
 }
---
+++ taskmanager/src/components/FormTask.jsx
@@ -0,0 +1,42 @@
+import React, {Component} from "react"
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+
+class FormTask extends Component {
+
+    state = {
+        task: null
+    };
+
+    render() {
+        return (
+            <div>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Group as={Col} md="4" controlId="validation1">
+                        <Form.Control
+                            required
+                            name="task"
+                            type="text"
+                            placeholder="Task"
+                            onChange={this.handleChange}
+                        />
+                        <Button type="submit">Add task</Button>
+                    </Form.Group>
+                </Form>
+            </div>
+        );
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+    };
+
+    handleChange = event => {
+        event.preventDefault();
+        const {name, value} = event.currentTarget;
+        this.setState({[name]: value});
+    };
+}
+
+export default FormTask;
