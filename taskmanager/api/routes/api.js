b46f13cca0314ccecb0ca934b48ad1a68d366482
jb087 <jb087@op.pl>
2020-02-29 11:20:03 +0100

    # Added db connection

--- taskmanager/api/routes/api.js
+++ taskmanager/api/routes/api.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+const mysql = require('mysql');
+const dbConnection = require('../db/dbConnection');
+
+const connection = mysql.createConnection(dbConnection);
+
 router.get('/', function (req, res, next) {
-    res.send('Hello world');
+    connection.connect(function (err) {
+        if (err) {
+            throw err;
+        }
+        console.log("Connected!");
+    });
+
+    res.status(200).json({
+        message: "HelloWorld!"
+    });
 });
 
 module.exports = router;
