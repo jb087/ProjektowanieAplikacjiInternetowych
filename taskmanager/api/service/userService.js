1abaa599f153433480cfe68a21fe4a27205ba296
jb087 <jb087@op.pl>
2020-03-05 16:22:18 +0100

    # Added schema of save tasks

--- taskmanager/api/service/userService.js
+++ taskmanager/api/service/userService.js
@@ -20,13 +20,13 @@ exports.checkUser = (res, requestBody) => {
     const user = User.withoutId(requestBody.login, requestBody.password);
     connection.query("SELECT COUNT(*) AS userCount FROM USER WHERE UserName = ? AND Password = ?", [user.login, user.password],
         function (err, result, fields) {
-        if (err) {
-            throw err;
-        }
+            if (err) {
+                throw err;
+            }
 
-        const isCorrectData = parseInt(result[0].userCount) === 1;
-        res.status(200).json({response: isCorrectData});
-    });
+            const isCorrectData = parseInt(result[0].userCount) === 1;
+            res.status(200).json({response: isCorrectData});
+        });
 };
 
 exports.userExists = (res, requestBody) => {
@@ -56,25 +56,55 @@ exports.registerUser = (res, requestBody) => {
 };
 
 exports.saveTasks = (res, login, requestBody) => {
-    const user = getUserByLogin(login);
-    const tasks = requestBody.map(task => new Task(task.id, task.task, task.taskContainerId, user.id));
-    console.log(tasks);
-
-    //TODO Save tasks and getUserByLogin must wait
-    //TODO jak task już jest na bazie, to go nie dodawać
-    //TODO zwrocić message
-
-    res.status(200).json({response: true});
-};
-
-async function getUserByLogin(login) {
-    let user = null;
-    await connection.query("SELECT * FROM USER WHERE UserName = ?", [login], function (err, result, fields) {
+    connection.query("SELECT * FROM USER WHERE UserName = ?", [login], function (err, result, fields) {
         if (err) {
             throw err;
         }
 
-        user = new User(result[0].ID, result[0].UserName, result[0].Password);
+        const user = new User(result[0].ID, result[0].UserName, result[0].Password);
+        const tasks = requestBody.map(task => new Task(task.id, task.task, task.taskContainerId, user.id));
+        connection.query("SELECT * FROM TASK", function (err, result, fields) {
+            if (err) {
+                throw err;
+            }
+
+            const ids = result.map(task => task.ID);
+            const tasksNotInDB = tasks.filter(task => !ids.includes(task.id));
+            const tasksInDB = tasks.filter(task => ids.includes(task.id));
+            if (tasksInDB.length > 0) {
+                tasksInDB.forEach(task => connection.query("DELETE FROM TASK WHERE ID = ?)", [task.id], function (err, result, fields) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    connection.query("INSERT INTO TASK (ID, TaskName, TaskContainerId, UserId) VALUES (?)", tasks, function (err, result, fields) {
+                        if (err) {
+                            throw err;
+                        }
+
+                        res.status(200).json({response: "Tasks was updated successfully!"})
+                    })
+                }));
+            } else {
+                const convertedTasks = tasksNotInDB.reduce((task1, task2) => {
+                    let array = [];
+                    array.push(task2.id);
+                    array.push(task2.taskName);
+                    array.push(task2.taskContainerId);
+                    array.push(task2.userId);
+                    task1.push(array);
+                    return task1;
+                }, []);
+                connection.query("INSERT INTO TASK (ID, TaskName, TaskContainerId, UserId) VALUES (?)", convertedTasks, function (err, result, fields) {
+                    //TODO dodaje tylko jeden task
+
+                    if (err) {
+                        throw err;
+                    }
+
+                    res.status(200).json({response: "New tasks added into database!"});
+                })
+            }
+        })
     });
-    return user;
-}
+};
